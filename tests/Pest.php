3f10fc91dc76ea59372735ac305c2e3cf0315c44
stephenjude <stephenjudesuccess@gmail.com>
2022-02-17 17:25:33 +0100

    Fixing failing tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,8 +1,8 @@
 <?php
 
 
-use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Stephenjude\FilamentBlog\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
-uses(LazilyRefreshDatabase::class)->in(__DIR__.'/Resources');
+uses(RefreshDatabase::class)->in(__DIR__.'/Resources');
