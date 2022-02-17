d32d5ed4cc2f66d52c96fffed2804c265b1aef9c
stephenjude <stephenjudesuccess@gmail.com>
2022-02-17 17:21:47 +0100

    Fixing test for workflow

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,6 +1,8 @@
 <?php
 
 
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Stephenjude\FilamentBlog\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
+uses(LazilyRefreshDatabase::class)->in(__DIR__.'/Resources');
