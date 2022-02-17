966cb5d15e0a49aa4aa5c2e926072d9e93821ed3
stephenjude <stephenjudesuccess@gmail.com>
2022-02-17 17:55:52 +0100

    Fixing test for workflow

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,6 +1,8 @@
 <?php
 
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Stephenjude\FilamentBlog\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
+uses(RefreshDatabase::class)->in(__DIR__.'/Resources');
