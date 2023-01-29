677d7312d75d39147550a35ab0adfd55cef9bfde
Chris Shortreed <chris@illusive.ch>
2023-01-29 08:11:11 -0700

    Cleaned up routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illusive\Blog\Http\Controllers\PostController;
 use Illusive\Blog\Http\Controllers\TagController;
 
-Route::group(['middleware' => config('jetstream.middleware', ['web'])], function () {
+Route::middleware(['web'])->group(function () {
     Route::prefix('blog')->group(function () {
         Route::resource('post', PostController::class)->parameters(['post' => 'slug']);
         Route::resource('tag', TagController::class)->parameters(['tag' => 'slug']);
